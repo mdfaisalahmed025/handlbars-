e78297a3e93637c9615265a5b173b50b30a57600
Md Faisal Ahmed <md.faisal3@northsouth.edu>
2022-12-07 01:43:03 +0600

    login register route

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
 require("dotenv").config();
-const path =require('path')
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-const hbs  = require('hbs')
+// const hbs  = require('hbs')
 
 // const html = require("html");
 
 const userRoute = require("./route/userRouter");
 const authroute = require("./route/authRouter");
 
-hbs.registerPartials(__dirname + 'views/partials')
+// hbs.registerPartials(__dirname + 'views/partials')
 
 //set det
-app.set('public', path.join(__dirname, '/views'))
-app.set('view engine', 'hbs')
-// app.set("view engine", "html");
+// app.set('public', path.join(__dirname, '/views'))
+// app.set('view engine', 'hbs')
+// // app.set("view engine", "html");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/user", userRoute);
 app.use("/auth", authroute);
-app.use(express.static("./public"));
+// app.use(express.static("./public"));
 
 const connectDB = require("./db/connect");
 
 app.get("/", (req, res) => {
-  res.render("index" ,{tittle: 'loginsystem' });
+  res.send("hello there");
 });
 
 app.get("/login", (req, res) => {
-  res.render("login", { tittle: "loginsystem" });
+  res.sendFile(__dirname + "/public/login.html");
 });
 
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/public/register.html");
+});
+
+//
 const PORT = process.env.PORT || 4000;
 
 const start = async () => {
